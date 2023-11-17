d2d5389f613d77e1efa3735ac67102c9b3f76968
Mar Kal <marc@example.com>
2023-11-17 15:49:48 +0100

    refactor: getProductById return product from merged products and stocks database

--- product-service/handlers/getProductsList.ts
+++ product-service/handlers/getProductsList.ts
@@ -1,4 +1,3 @@
-import { mockedProducts } from "../data/products";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 import { buildResponse } from "../data/libs/utils";
