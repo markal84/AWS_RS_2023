e106922a0f19e0667d65e627ccd25d8792f00144
Mar Kal <marc@example.com>
2023-12-13 19:29:09 +0100

    feat: create authorization stack

--- authorization-service/bin/authorization-service.ts
+++ authorization-service/bin/authorization-service.ts
@@ -1,21 +1,36 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { AuthorizationServiceStack } from '../lib/authorization-service-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { config } from "dotenv";
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+config();
+
+const { AWS_REGION } = process.env;
 
 const app = new cdk.App();
-new AuthorizationServiceStack(app, 'AuthorizationServiceStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const stack = new cdk.Stack(app, "AuthorizationServiceStack", {
+  env: { region: AWS_REGION! },
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const lambdaProps: Partial<NodejsFunctionProps> = {
+  runtime: lambda.Runtime.NODEJS_18_X,
+  environment: {
+    PRODUCT_AWS_REGION: AWS_REGION!,
+  },
+};
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const basicAuthorizer = new NodejsFunction(stack, "basicAuthorizerLambda", {
+  ...lambdaProps,
+  functionName: "basicAuthorizer",
+  entry: "handlers/basicAuthorizer.ts",
+});
+
+new cdk.CfnOutput(stack, "BasicAuthorizerLambdaArn", {
+  value: basicAuthorizer.functionArn,
 });
